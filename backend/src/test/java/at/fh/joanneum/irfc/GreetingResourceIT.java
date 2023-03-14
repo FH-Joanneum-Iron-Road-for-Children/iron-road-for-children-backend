6da03848976b7ecb97ce356f55e748ff173f3ea7
Philip Moser <philip.moser@edu.fh-joanneum.at>
2023-03-14 17:36:00 +0100

    #1 create quarkus plain project

---
+++ backend/src/test/java/at/fh/joanneum/irfc/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package at.fh.joanneum.irfc;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
