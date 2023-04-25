4346269acd909ca50aace45aeb0b46d3cf093098
Lion Kornsteiner <kornsteinerl@gmail.com>
2023-04-25 11:15:26 +0200

    basic firebase implementation

--- src/main/java/at/fh/joanneum/irfc/rest/EventApi.java
+++ src/main/java/at/fh/joanneum/irfc/rest/EventApi.java
@@ -2,13 +2,25 @@ package at.fh.joanneum.irfc.rest;
 
 import at.fh.joanneum.irfc.model.event.EventDTO;
 import at.fh.joanneum.irfc.service.EventService;
+import io.smallrye.jwt.auth.principal.DefaultJWTCallerPrincipal;
 
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Set;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
 
 /**
  * @author moe@softwaregaertner.at
@@ -19,6 +31,39 @@ public class EventApi {
 
   @Inject
   EventService eventService;
+
+  //TODO remove hello and claimNames endpoint
+
+  @GET
+  @Path("/secure")
+  @RolesAllowed("admin")
+  public String secureEndpoint() {
+    // This endpoint requires the "admin" role.
+    return "Hello, authenticated user!";
+  }
+  @GET
+  @Path("hello")
+  @Produces(MediaType.TEXT_PLAIN)
+  @PermitAll
+  public String hello(@Context SecurityContext securityContext) {
+    final var user = (DefaultJWTCallerPrincipal) securityContext.getUserPrincipal();
+    if (user == null){
+      return "Hello Anonymous User";
+    }
+    final var email = user.getClaim("email");
+    return "hello " + email;
+  }
+
+  @GET
+  @Path("claim_names")
+  @Produces(MediaType.TEXT_PLAIN)
+  @PermitAll
+  public Set<String> claimNames(@Context SecurityContext securityContext) {
+    final var user = (DefaultJWTCallerPrincipal) securityContext.getUserPrincipal();
+    if (user == null)
+      return Set.of();
+    return user.getClaimNames();
+  }
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
