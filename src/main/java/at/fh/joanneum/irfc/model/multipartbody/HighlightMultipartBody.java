83e936107c2fcc2536d6f431d51eb7950e5ae5b7
David Krall <david.krall@edu.fh-joanneum.at>
2025-03-30 16:58:20 +0200

    added highlight service and api

---
+++ src/main/java/at/fh/joanneum/irfc/model/multipartbody/HighlightMultipartBody.java
@@ -0,0 +1,35 @@
+package at.fh.joanneum.irfc.model.multipartbody;
+
+import at.fh.joanneum.irfc.persistence.enums.FileEndingType;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.jboss.resteasy.annotations.providers.multipart.PartType;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.core.MediaType;
+import java.io.InputStream;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@RegisterForReflection
+public class HighlightMultipartBody {
+
+    @FormParam("file")
+    @PartType(MediaType.APPLICATION_OCTET_STREAM)
+    public InputStream file;
+
+    @FormParam("altText")
+    @PartType(MediaType.TEXT_PLAIN)
+    public String altText;
+
+    @FormParam("description")
+    @PartType(MediaType.TEXT_PLAIN)
+    public String description;
+
+    @FormParam("fileType")
+    @PartType(MediaType.TEXT_PLAIN)
+    public FileEndingType fileEndingType;
+}
