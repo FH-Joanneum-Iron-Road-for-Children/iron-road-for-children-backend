9a3fbf498aca02405e51d99865a681a127a74870
max-pfi <m1999.pfisterer@gmail.com>
2025-04-26 10:41:04 +0200

    Update swagger docu

--- src/main/java/at/fh/joanneum/irfc/rest/PlaylistApi.java
+++ src/main/java/at/fh/joanneum/irfc/rest/PlaylistApi.java
@@ -2,6 +2,8 @@ package at.fh.joanneum.irfc.rest;
 
 import at.fh.joanneum.irfc.model.playlist.PlaylistDTO;
 import at.fh.joanneum.irfc.service.PlaylistService;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -22,17 +24,29 @@ public class PlaylistApi {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
+    @APIResponses({
+            @APIResponse(responseCode = "200", description = "Playlist created or updated"),
+            @APIResponse(responseCode = "400", description = "Invalid input")
+    })
     public PlaylistDTO createOrUpdate(PlaylistDTO playlistDTO) {
         return playlistService.createOrUpdate(playlistDTO);
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @APIResponses({
+            @APIResponse(responseCode = "200", description = "Playlist found"),
+            @APIResponse(responseCode = "204", description = "No playlist found")
+    })
     public PlaylistDTO get() {
         return playlistService.getSingle();
     }
 
     @DELETE
+    @APIResponses({
+            @APIResponse(responseCode = "200", description = "Playlist deleted"),
+            @APIResponse(responseCode = "404", description = "No playlist to delete")
+    })
     @Produces(MediaType.APPLICATION_JSON)
     public Response delete() {
         boolean deleted = playlistService.delete();
