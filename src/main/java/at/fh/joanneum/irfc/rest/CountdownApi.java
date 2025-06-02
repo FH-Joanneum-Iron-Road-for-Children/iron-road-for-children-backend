c0ae498367bb8e0ecad57e2fa6588354232cb9bb
David Krall <david.krall@edu.fh-joanneum.at>
2025-06-02 12:49:35 +0200

    removed authentication for countdown api

--- src/main/java/at/fh/joanneum/irfc/rest/CountdownApi.java
+++ src/main/java/at/fh/joanneum/irfc/rest/CountdownApi.java
@@ -2,7 +2,6 @@ package at.fh.joanneum.irfc.rest;
 
 import at.fh.joanneum.irfc.model.countdown.CountdownDTO;
 import at.fh.joanneum.irfc.service.CountdownService;
-import io.quarkus.security.Authenticated;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -21,7 +20,6 @@ public class CountdownApi {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Authenticated
     public CountdownDTO getCountdown() {
         return countdownService.getCountdown();
     }
@@ -29,7 +27,6 @@ public class CountdownApi {
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    @Authenticated
     public CountdownDTO update(CountdownDTO countdownDTO) {
 
         return countdownService.update(countdownDTO);
